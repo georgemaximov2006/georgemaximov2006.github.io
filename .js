4a52efdd6c93072e57195c2f22e9d04cb8802fb5
georgemaximov2006 <gosham2007@gmail.com>
2021-11-27 16:04:42 +0500

    1.0

---
+++ .js
@@ -0,0 +1,12 @@
+var x = 5;
+var y = [65, 75, "Hi"];
+var z = {
+    form: "Куб",
+    color: "Yellow",
+    sayHi: function(){
+        alert(var z);
+    }
+};
+z.form = "Шар"
+z.color = "Red"
+z.sayHi();
---
+++ script12.js
@@ -0,0 +1,92 @@
+/*var x = 2;
+
+while(x < 128)
+    {
+        alert(x)
+        x = x * 2;
+        if (x == 128)
+        {
+            alert(x);
+            alert("Hello");
+        }
+        
+    }
+
+// Конец первого кода
+
+    var z = 50;
+    var c = 50; 
+    
+    z = prompt("Введите первое число");
+
+    c = prompt("Введите второе число");
+
+    if (z > c)
+        {
+            alert("Получилось");
+        }
+    else if (z < c)
+        {
+            alert("Не получилось");
+        }
+    else 
+        {
+             alert("Ошибка");
+        }
+// Конец второго кода
+*/
+
+function one()
+{
+    alert("Hello");
+}
+function two()
+{
+    alert("Bye");
+}function three()
+{
+    alert("Good night");
+}
+
+three();
+two();
+three();
+one();
+two();
+three();
+two();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
