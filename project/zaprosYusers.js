a1da4de06991d79d68b04b1b6f1647e802fa6928
georgemaximov2006 <gosham2007@gmail.com>
2021-11-27 16:35:35 +0500

    1.1

---
+++ project/zaprosYusers.js
@@ -0,0 +1,22 @@
+var x = 5;
+var y = 10;
+
+x = prompt("Введите число:")
+y = prompt("Введите второе число:")
+
+while (x = y)
+{
+    alert("x < y");
+}
+if (x >= y)
+{
+    alert("x >= y");
+}
+else if (x <= y)
+{
+    alert("x <= y");
+}
+else (x < y)
+{
+    alert("x < y");
+}
